aadc6af217d4f2fb14c4996b2fe0fc091f4bab23
Gustavo Starace <kaaslange@hotmail.com>
2023-01-16 19:16:45 +0100

    Ya sonidos cargados

--- css/JS/motorcito.js
+++ css/JS/motorcito.js
@@ -1,12 +1,16 @@
 var estadoLicuadora = "apagada";
 var licuadora = document.getElementById("blender");
+var sonidoLicuadora = document.getElementById("blender-sound");
+var botonLicuadora = document.getElementById("blender-button.sound")
 
 function controlarLicuadora() {
     if (estadoLicuadora == "apagada") {
-        estadoLicuadora = "encendido"
+        estadoLicuadora = "encendido";
+        licuadora.classList.add("active")
         console.log("me prendiste")
     } else {
         estadoLicuadora = "apagada"
-        console.log("me apagaste")
+        console.log("me apagaste");
+        licuadora.classList.remove("active")
     }
 }
